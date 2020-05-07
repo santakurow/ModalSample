645b028b9ffcba826d22dc67f1bdcd985d3ecd3c
santakurow <newtakuproj0730@gmail.com>
2020-05-08 05:15:10 +0900

    delete appendHTML in modal.js

--- app/javascript/packs/modal.js
+++ app/javascript/packs/modal.js
@@ -1,25 +1,8 @@
 
 $(function () {
 
-  let appendHTML = `<div id="modal">
-                      <div id="modal-window">
-                        <div id="modal-window__title">
-                          <h3>Title</h3>
-                        </div>
-                        <div id="modal-window__close">
-                          <span class="close">X</span>
-                        </div>
-                        <div id="modal-window__desc">
-                          <p>Hello everyone! Hello everyone!Hello everyone!Hello everyone!Hello everyone!</p>
-                        </div>
-                      </div>
-                    </div>`
-
-
   $(".modal-action__btn").on("click", function (e) {
     e.preventDefault();
-    // $("#modal-wrapper").append(appendHTML);
-    // $("#modal-window").css("tranform", "translate(0, -200px)");
     $("#modal").css("display", "block");
     $("#modal-window").animate({ display: "block" }, {
       start: function () {
@@ -38,10 +21,6 @@ $(function () {
   }
   
   $("#modal").on("click", function (e) {
-    // let window = $("#modal-window");
-    // let offset = $(window).offset();
-    // let width = $(window).width();
-    // let height = $(window).height();
     if (!isModalWindow(e)) {
       $(this).css("display", "none");
       $("#modal-window").removeAttr("style");
@@ -51,8 +30,4 @@ $(function () {
     }
     
   })
-  // $(document).on("submit", "#new_user", function (e) {
-  //   e.preventDefault();
-  //   alert("success");
-  // })
 })
